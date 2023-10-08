647922f2ee900f569391b2dec27673b8eb1e505a
MahshadOliaie <mahshadoliaie@gmail.com>
2023-10-08 17:40:08 +0330

    add cinemas

--- assets/js/requests.js
+++ assets/js/requests.js
@@ -12,3 +12,10 @@ function getOneMovie(index){
     .then(res => res.json())
     .then(data => showMovie(data[index]))
 }
+
+
+function getCinemas(){
+    fetch("assets/cinemas.json")
+    .then(res=> res.json())
+    .then(data=> renderCinemas(data))
+}

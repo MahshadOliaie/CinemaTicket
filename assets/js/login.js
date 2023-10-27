8ee2ee20dcf791febf79a6923322169101910ec1
MahshadOliaie <mahshadoliaie@gmail.com>
2023-10-27 14:35:16 +0330

    add animation background for login and signup pages

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,7 +1,7 @@
 
 
 function login() {
-  let template = `<div class="flex container max-w-sm h-auto flex-col justify-center px-6 py-12 lg:px-8" style="background-color:rgba(29, 0, 29, 0.952)">
+  let template = bgTemplate + `<div class="formBox flex container max-w-sm h-auto flex-col justify-center px-6 py-12 lg:px-8" style="background-color:rgba(29, 0, 29, 0.952)">
     <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-10 w-auto" src="./assets/images/logo.png" alt="Your Company">
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-purple-500">Sign in to your account</h2>
@@ -27,7 +27,6 @@ function login() {
             <input id="password" name="password" type="password" autocomplete="current-password" required class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 bg-purple-200 focus:ring-2 focus:ring-inset focus:ring-purple-400 sm:text-sm sm:leading-6 px-2">
           </div>
         </div>
-  
         <div>
           <button type="submit" class="flex w-full justify-center rounded-md bg-purple-500 px-3 py-1.5 text-sm font-semibold leading-6 text-purple-100 shadow-sm hover:bg-purple-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-purple-500">Sign in</button>
         </div>
@@ -54,7 +53,7 @@ function login() {
 
 
 function signUp() {
-  let template = `<div class="flex container max-w-sm h-auto flex-col justify-center px-6 py-12 lg:px-8" style="background-color:rgba(29, 0, 29, 0.952)">
+  let template = bgTemplate + `<div class="formBox flex container max-w-sm h-auto flex-col justify-center px-6 py-12 lg:px-8" style="background-color:rgba(29, 0, 29, 0.952)">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
       <img class="mx-auto h-10 w-auto" src="./assets/images/logo.png" alt="Your Company">
       <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-purple-500">Create an account</h2>
